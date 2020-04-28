4f0214181c164a94518324f923d2338550de3ac1
Logan Scanlon <scan3ls@gmail.com>
2020-04-28 23:37:38 +0000

    Add task and Update README

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+str1 = "and is greater than 5"
+str2 = "and is less than 6 and not 0"
+if number % 10 == 0:
+    print("Last digit of {} is {} and is 0".format(number, (number % 10)))
+elif number % 10 > 5:
+    print("Last digit of {} is {} {}".format(number, (number % 10), str1))
+else:
+    print("Last digit of {} is {} {}".format(number, (number % 10), str2))
--- 0x01-python-if_else_loops_functions/README.md
+++ 0x01-python-if_else_loops_functions/README.md
@@ -4,3 +4,4 @@
 |File| Description|
 |-|-|
 |0-positive_or_negative.py| Determine and print whether a number is positive or negative|
+|1-last_digit.py| Determine and print last number, ones place, of a random number|
