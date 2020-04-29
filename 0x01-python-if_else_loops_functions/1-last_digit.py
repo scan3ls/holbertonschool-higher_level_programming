253f6bf28af5a4d469dac18142f6bbb32ce35c66
Logan Scanlon <scan3ls@gmail.com>
2020-04-29 03:40:36 +0000

    Fix issue with negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+num2 = number
 str1 = "and is greater than 5"
 str2 = "and is less than 6 and not 0"
-if number < 0:
-    number *= -1
-if number % 10 == 0:
-    print("Last digit of {} is {} and is 0".format(number, (number % 10)))
-elif number % 10 > 5:
-    print("Last digit of {} is {} {}".format(number, (number % 10), str1))
+if num2 < 0:
+    num2 *= -1
+if num2 % 10 == 0:
+    print("Last digit of {} is {} and is 0".format(number, (num2 % 10)))
+elif num2 % 10 > 5:
+    print("Last digit of {} is {} {}".format(number, (num2 % 10), str1))
 else:
-    print("Last digit of {} is {} {}".format(number, (number % 10), str2))
+    print("Last digit of {} is {} {}".format(number, (num2 % 10), str2))
