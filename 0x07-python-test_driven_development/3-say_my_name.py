9b6a65820d20ee030a57d2ecc5f07e32a77d11b6
Logan Scanlon <scan3ls@gmail.com>
2020-05-21 13:54:07 +0000

    Add task

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""prints a name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """prints a name
+
+    Args:
+        first_name: first name
+        last_name: last name
+    
+    """
+
+    if first_name is None:
+        raise TypeError("say_my_name() missing 1 required positional argument: 'first_name'")
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
