6ae5c75dcc7a3f28503ed7efb498c6ba2a95a649
Logan Scanlon <scan3ls@gmail.com>
2020-07-14 22:41:54 +0000

    Add task

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- lists all cities of California in the database
+-- comment 2
+SELECT
+    cities.id
+	,cities.name
+    FROM cities, states
+	WHERE cities.state_id = states.id
+		AND states.name LIKE 'California'
+    ORDER BY id;
