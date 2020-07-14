a70a8b22efe51d7cf0d950670aee6bf2a815e885
Logan Scanlon <scan3ls@gmail.com>
2020-07-14 20:00:27 +0000

    Change user name

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- creates mysql user user_0d_1
 -- comment 2
 DROP USER IF EXISTS 'user_0d_1'@'localhost';
-CREATE USER 'jeffrey'@'localhost'
+CREATE USER 'user_0d_1'@'localhost'
 	IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
