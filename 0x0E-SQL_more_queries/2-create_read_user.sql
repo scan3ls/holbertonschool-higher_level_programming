9b6fa5c1aa453f102b615c4a661ebded0b4c7b72
Logan Scanlon <scan3ls@gmail.com>
2020-07-14 20:27:12 +0000

    Add backticks around database

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 -- create db hbtn_0d_2 and user user_0d_2
 -- comment 2
 DROP USER IF EXISTS 'user_0d_2'@'localhost';
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER 'user_0d_2'@'localhost'
 	IDENTIFIED BY 'user_0d_1_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_1'@'localhost';
+GRANT SELECT ON `hbtn_0d_2.*` TO 'user_0d_1'@'localhost';
