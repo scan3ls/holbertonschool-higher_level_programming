bf316cae3095bf945058fb727b01374bfdc39a2e
Logan Scanlon <scan3ls@gmail.com>
2020-07-14 20:25:37 +0000

    Change user in drop statement

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- create db hbtn_0d_2 and user user_0d_2
 -- comment 2
-DROP USER IF EXISTS user_0d_2;
+DROP USER IF EXISTS 'user_0d_2'@'localhost';
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER 'user_0d_2'@'localhost'
 	IDENTIFIED BY 'user_0d_1_pwd';
