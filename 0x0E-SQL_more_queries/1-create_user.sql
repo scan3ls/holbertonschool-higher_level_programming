13aca89431ecdce015560dd92de1747c5ca621b8
Logan Scanlon <scan3ls@gmail.com>
2020-07-14 19:57:33 +0000

    Remove users if exists

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,6 @@
 -- creates mysql user user_0d_1
 -- comment 2
-CREATE USER IF NOT EXISTS
-	'user_0d_1'@'localhost';
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
+CREATE USER 'jeffrey'@'localhost'
+	IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
