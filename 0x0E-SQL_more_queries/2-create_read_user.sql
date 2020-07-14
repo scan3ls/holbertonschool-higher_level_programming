1ff0043943f6f57dc611e794ec3d758899558b4b
Logan Scanlon <scan3ls@gmail.com>
2020-07-14 20:32:44 +0000

    correct wildcard placement

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,4 +4,4 @@ DROP USER IF EXISTS 'user_0d_2'@'localhost';
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER 'user_0d_2'@'localhost'
 	IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON `hbtn_0d_2.*` TO 'user_0d_2'@'localhost';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
