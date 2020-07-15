9467ee80dc50f107891de6872cabd6881fea3b55
Logan Scanlon <scan3ls@gmail.com>
2020-07-15 16:57:32 +0000

    Add task

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,2 +1,16 @@
 -- lists all shows and all genres lined to the show-- lists all shows w/ at least one genre linked
 -- comment 2
+SELECT 
+	tv_shows.title
+	,tv_genres.name
+	FROM tv_shows 
+	
+	LEFT JOIN tv_show_genres 
+	ON tv_shows.id = tv_show_genres.show_id 
+
+	LEFT JOIN tv_genres 
+	ON tv_show_genres.genre_id = tv_genres.id 
+
+	ORDER BY
+		tv_shows.title
+		,tv_genres.name
