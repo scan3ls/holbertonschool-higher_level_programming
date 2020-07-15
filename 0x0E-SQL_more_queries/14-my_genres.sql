2fc2ea9b4cbe5bf3cac553ecf8c4496e7b63d033
Logan Scanlon <scan3ls@gmail.com>
2020-07-15 16:15:02 +0000

    Add task

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,13 @@
 -- list all genres of the show Dexter
 -- comment 2
-SELECT DISTINCT
+SELECT 
+	tv_genres.name 
+	FROM tv_show_genres 
 	
+	JOIN tv_shows 
+	ON tv_show_genres.show_id = tv_shows.id 
+	
+	JOIN tv_genres 
+	ON tv_show_genres.genre_id = tv_genres.id 
+	
+	WHERE tv_shows.title = "Dexter";
