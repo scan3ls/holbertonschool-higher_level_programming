590af1e49c215a1c592e62db598a7d6304537390
Logan Scanlon <scan3ls@gmail.com>
2020-07-15 16:21:15 +0000

    Add task

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,2 +1,13 @@
 -- lists all Comedy shows
 -- comment 2
+SELECT 
+	tv_shows.title 
+	FROM tv_show_genres 
+	
+	JOIN tv_shows 
+	ON tv_show_genres.show_id = tv_shows.id 
+	
+	JOIN tv_genres 
+	ON tv_show_genres.genre_id = tv_genres.id 
+	
+	WHERE tv_genres.name = "Comedy";
