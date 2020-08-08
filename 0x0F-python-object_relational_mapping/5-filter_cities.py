e675ae65e67df7f3b53fb07049d26403818db45d
Logan Scanlon <scan3ls@gmail.com>
2020-08-08 02:46:53 +0000

    Add task

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+""" module """
+
+if __name__ == "__main__":
+
+    import MySQLdb
+
+    # if script takes args
+    from sys import argv
+
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    stateName = argv[4]
+
+    # end if
+
+    # MySQLdb connect() args
+    params = {
+        "user": username,  # Defaults to current user
+        "passwd": password,  # Defaults No password
+        "db": database,  # no Default
+    }
+
+    # MySQLdb connect to database
+    db = MySQLdb.connect(**params)
+
+    # Setup cursor to execute MySQL commands
+    cursor = db.cursor()
+    cursor.execute(
+        """
+        SELECT cities.id, cities.name, states.name
+            FROM cities
+            JOIN states
+            ON cities.state_id = states.id
+            WHERE states.name = %s
+            ORDER BY cities.id
+        """, (stateName,)
+    )
+
+    # collect the stored results
+    results = cursor.fetchall()
+
+    # use the results
+
+    # python code goes here #
+    cities = []
+    for item in results:
+        cities.append(item[1])
+    print(*cities, sep=", ")
+    
+    # close cursor
+    cursor.close()
+
+    # close connection to database
+    db.close()
