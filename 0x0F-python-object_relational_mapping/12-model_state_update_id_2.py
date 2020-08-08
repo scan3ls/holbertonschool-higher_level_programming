714eaa67ffc5e3563a66dc25a17e88c2ca1d6556
Logan Scanlon <scan3ls@gmail.com>
2020-08-08 16:31:29 +0000

    Add task

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" module """
+
+if __name__ == "__main__":
+
+    # imports
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy.ext.declarative import declarative_base
+
+    # creates the engine
+    # format("user", "passwd", "db_name")
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ), pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
+
+    # creates the session
+    Session = sessionmaker(bind=engine)
+
+    # creates declarative base
+    Base = declarative_base()
+
+    # use base to create relation with db_table
+
+    # connect to the db
+    session = Session()
+
+    # perform the sql command
+    change_me = session.query(State).filter(State.id == 2)
+    state = change_me.first()
+    state.name = "New Mexico"
+    session.add(state)
+    session.commit()
---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" module """
+
+if __name__ == "__main__":
+
+    # imports
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy.ext.declarative import declarative_base
+
+    # creates the engine
+    # format("user", "passwd", "db_name")
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ), pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
+
+    # creates the session
+    Session = sessionmaker(bind=engine)
+
+    # creates declarative base
+    Base = declarative_base()
+
+    # use base to create relation with db_table
+
+    # connect to the db
+    session = Session()
+
+    # perform the quarry
+    things = session.query(State)
+    first = things.first()
+    if first is not None:
+        print("{}: {}".format(first.id, first.name))
