bc07644732d9f2b7db4cd411daefc557ec6426f2
Logan Scanlon <scan3ls@gmail.com>
2020-08-08 00:56:14 +0000

    Add templates

---
+++ 0x0F-python-object_relational_mapping/mysqldb_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" module """
+
+if __name__ = "__main__":
+
+    import MySQLdb
+
+    # if script takes args
+    from sys import argv
+
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    # end if
+
+    # MySQLdb connect() args
+    params = {
+        "host": "ph_host",  # Defaults to local host
+        "user": "ph_user",  # Defaults to current user
+        "passwd": "ph_password",  # Defaults No password
+        "db": "ph_database",  # no Default
+        "port": int("ph_port"),  # Defaults to 3306
+        "conv": "ph conversion dict"  # Default: MySQLdb.converters.conversions
+    }
+
+    # MySQLdb connect to database
+    db = MySQLdb.connect(**params)
+
+    # Setup cursor to execute MySQL commands
+    cursor = db.cursor()
+    cursor.execute(
+        # mysql command goes here
+    )
+
+    # collect the stored results
+    results = cursor.fetchall()
+
+    # use the results
+
+    # python code goes here #
+    for item in results:
+        print(item)
+
+    # close cursor
+    cursor.close()
+
+    # close connection to database
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/sqlalchemy_template.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" module """
+
+if __name__ = "__main__":
+
+    # imports
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import Column, Integer, String
+
+    # creates the engine
+    # format("user", "passwd", "db_name")
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        "root", "", "hbtn_0d_usa"
+    ), pool_pre_ping=True)
+
+    # creates the session
+    Session = sessionmaker(bind=engine)
+
+    # creates declarative base
+    from sqlalchemy.ext.declarative import declarative_base
+    Base = declarative_base()
+
+    # use base to create relation with db_table
+    class table(Base):
+        """ class """
+        __tablename__ = 'states'
+
+        id = Column(Integer, primary_key=True)
+        name = Column(String)
+
+        def __repr__(self):
+            """ function """
+            return "<State(name={},  id={}".format(
+                self.name, self.id
+            )
+
+    # connect to the db
+    session = Session()
+
+    # perform the quarry
+    for thing in session.query(table):
+        print("({}, '{}')".format(thing.id, thing.name))
