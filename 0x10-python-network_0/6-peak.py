eec5f781c9b8f5ead2d8da285a4fdcfa5ae759da
scan3ls <scan3ls@gmail.com>
2020-08-18 21:27:21 +0000

    Reduce calls to len

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,10 +11,10 @@ def find_peak(list_of_integers):
     if list_of_integers == []:
         return None
 
-    if len(list_of_integers) == 1:
+    length = len(list_of_integers)
+    if length == 1:
         return list_of_integers[0]
 
-    length = len(list_of_integers)
     mid = int(length / 2)
     start = list_of_integers[mid]
 
