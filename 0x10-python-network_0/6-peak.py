20908abea45b3b4ebf7db1933fb0e9e35c7cb7f6
scan3ls <scan3ls@gmail.com>
2020-08-18 21:13:24 +0000

    Add task

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Peak finder """
+
+def find_peak(list_of_integers):
+    """ finder """
+
+    if list_of_integers is None:
+        return None
+    if type(list_of_integers) is not list:
+        return None
+    if list_of_integers == []:
+        return None
+
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+
+    length = len(list_of_integers)
+    mid = int(length / 2)
+    start = list_of_integers[mid]
+
+    current = start
+    max_right = find_peak(list_of_integers[mid:])
+    max_left = find_peak(list_of_integers[:mid])
+
+    if max_left < current > max_right:
+        return current
+    elif current < max_left > max_right:
+        return max_left
+    else:
+        return max_right
