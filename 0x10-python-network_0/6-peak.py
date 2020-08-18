388099f5d5ca880a35285c2b2fb9b516626b9b3b
scan3ls <scan3ls@gmail.com>
2020-08-18 21:24:21 +0000

    Add a check for none

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -22,6 +22,9 @@ def find_peak(list_of_integers):
     max_right = find_peak(list_of_integers[mid:])
     max_left = find_peak(list_of_integers[:mid])
 
+    if max_left is None or current is None or max_right is None:
+        return None
+
     if max_left < current > max_right:
         return current
     elif current < max_left > max_right:
