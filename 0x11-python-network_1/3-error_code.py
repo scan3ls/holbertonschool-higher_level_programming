6373cd0d4211d55cd942f4184977534913f6303f
scan3ls <scan3ls@gmail.com>
2020-08-24 08:58:28 +0000

    Add task

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.error
+    from sys import argv
+
+    url = argv[1]
+
+    req = urllib.request.Request(url)
+
+    try:
+        with urllib.request.urlopen(url) as r:
+            html = r.read()
+        html = html.decode('UTF-8')
+        print(html)
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
