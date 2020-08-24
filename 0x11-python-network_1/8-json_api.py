b6b964374e7485937ec5d243db322b6d89b16c56
scan3ls <scan3ls@gmail.com>
2020-08-24 16:23:55 +0000

    Add task

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3                                                                            
+""" module """                                                                                
+                                                                                              
+                                                                                              
+if __name__ == "__main__":                                                                    
+    from sys import argv                                                                      
+    import requests                                                                           
+                                                                                              
+    url = "http://0.0.0.0:5000/search_user"                                                   
+                                                                                              
+    if len(argv) <= 1:                                                                        
+        q = ""                                                                                
+    else:                                                                                     
+        q = argv[1]                                                                           
+                                                                                              
+    r = requests.post(url, data = {'q': q})                                                   
+    dictionary = r.json()                                                                     
+    if dictionary == {}:                                                                      
+        print("No result")                                                                    
+        exit()                                                                                
+                                                                                              
+    id = dictionary['id']                                                                     
+    name = dictionary['name']                                                                 
+    print("[{}] {}".format(id, name))
