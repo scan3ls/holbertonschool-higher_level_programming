7855737fcd4bca7468e686a0c6b86efe00374380
scan3ls <scan3ls@gmail.com>
2020-08-24 08:43:20 +0000

    Add task

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" module """
+
+
+def post_request():
+    """ send post request to url with email """
+    import urllib.request as req
+    import urllib.parse as parse
+    from sys import argv
+
+    url = argv[1]
+    email = argv[2]
+
+    values = {
+        'email': email
+    }
+
+    data = parse.urlencode(values)
+    data = data.encode('ascii')
+    request = req.Request(url, data)
+
+    with req.urlopen(request) as response:
+        page = response.read()
+    page = page.decode('UTF-8')
+
+
+if __name__ == "__main__":
+    post_request()
