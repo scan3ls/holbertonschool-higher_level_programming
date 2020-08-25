9fa1fb5190aedadbd3a6f04c6cb927260787b597
scan3ls <scan3ls@gmail.com>
2020-08-25 03:14:34 +0000

    Change print statement

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     type_cont = type(content)
     print("Body response:")
     print("\t- type:", type_cont)
-    print("\t- content", content)
+    print("\t- content:", content)
     # print("\t- utf8 content:", content.decode('UTF-8'))
