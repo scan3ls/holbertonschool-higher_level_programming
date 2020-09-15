b5291dd98cba9ff7079461f964c16102e6406eef
scan3ls <scan3ls@gmail.com>
2020-09-15 07:34:03 +0000

    Fix style

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.add = function add (a, b) {
-    if (isNaN(a) || isNaN(b)) {
-      return NaN;
-    } else {
-      return a + b;
-    }
+  if (isNaN(a) || isNaN(b)) {
+    return NaN;
+  } else {
+    return a + b;
   }
+};
