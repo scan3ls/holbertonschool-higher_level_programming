1f84d22400bbc6ae3c0b015d16631aca0d6b5274
scan3ls <scan3ls@gmail.com>
2020-09-16 03:49:40 +0000

    Print '0' as a string

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ const numbers = args.splice(2);
 const second = numbers.sort().reverse()[1];
 
 if (isNaN(parseInt(second))) {
-  console.log(0);
+  console.log('0');
 } else {
   console.log(second);
 }
