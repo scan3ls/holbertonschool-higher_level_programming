ffbb711568eff551548958dd3f9989a33dd05550
scan3ls <scan3ls@gmail.com>
2020-09-21 20:56:33 +0000

    Let class return empty object

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -3,11 +3,11 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     if (isNaN(w) || isNaN(h)) {
-      this.width = 0;
-      this.height = 0;
+      // this.width = 0;
+      // this.height = 0;
     } else if (w <= 0 || h <= 0) {
-      this.width = 0;
-      this.height = 0;
+      // this.width = 0;
+      // this.height = 0;
     } else {
       this.width = w;
       this.height = h;
