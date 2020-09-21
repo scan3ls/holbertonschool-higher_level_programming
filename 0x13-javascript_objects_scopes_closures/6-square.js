b435c703eff072bc8f091fffdd8fe658f83759b5
scan3ls <scan3ls@gmail.com>
2020-09-21 20:47:12 +0000

    Changes Extends

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
-module.exports = class Square extends Rectangle {
+const square = require('./5-square');
+module.exports = class Square extends square {
   constructor (size) {
     super(size, size);
   }
