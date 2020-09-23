05fe54a64ded417d956484d478b551a62123c81b
scan3ls <scan3ls@gmail.com>
2020-09-23 15:26:24 +0000

    fix typo

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -5,12 +5,13 @@ const dict = require('./101-data.js').dict;
 const newDict = {};
 
 for (const key in dict) {
-  if (dict[key] in dict) continue;
+  if (dict[key] in newDict) continue;
   else newDict[dict[key]] = [];
 }
 
 for (const key in dict) {
   const newKey = dict[key];
+  //console.log(newDict[newKey])
   newDict[newKey].push(key);
 }
 
